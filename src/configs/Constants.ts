9887eb2897ac74bd0d7813d463eb2166ac038479
Balaji <balaji00710@gmail.com>
2022-09-16 23:13:44 +0530

    Added Constants

---
+++ src/configs/Constants.ts
@@ -0,0 +1,12 @@
+export const MenuLinks = {
+    "AboutMe": "/",
+    "Experience": "/experience",
+    "TechStacks": "/techstacks"
+}
+
+export const SiteContents = {
+    "SiteName": "Balaji Premkumar",
+    "Position": "Full Stack Engineer",
+    "Description": "Full stack Engineer with 9+ years of expertise in application layers, presentation layers, databases, and servers. " +
+        "Extensive experience in both FE and BE, as well as server technologies."
+}
